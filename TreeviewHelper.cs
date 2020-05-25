cc20b437dc2c1afa7a640bd3a87a9d7a68ede479
Terry <tcdj1963@gmail.com>
2020-05-26 07:45:22 +1200

    Add files via upload

---
+++ TreeviewHelper.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AssetManager
+{
+    class TreeviewHelper
+    {
+    
+    private TreeViewItem GetTreeViewItem(ItemsControl container, object item)
+    {
+        if (container != null)
+        {
+            if (container.DataContext == item)
+            {
+                return container as TreeViewItem;
+            }
+
+            // Expand the current container
+            if (container is TreeViewItem && !((TreeViewItem)container).IsExpanded)
+            {
+                container.SetValue(TreeViewItem.IsExpandedProperty, true);
+            }
+
+            // Try to generate the ItemsPresenter and the ItemsPanel.
+            // by calling ApplyTemplate.  Note that in the 
+            // virtualizing case even if the item is marked 
+            // expanded we still need to do this step in order to 
+            // regenerate the visuals because they may have been virtualized away.
+
+            container.ApplyTemplate();
+            ItemsPresenter itemsPresenter = (ItemsPresenter)container.Template.FindName("ItemsHost", container);
+            if (itemsPresenter != null)
+            {
+                itemsPresenter.ApplyTemplate();
+            }
+            else
+            {
+                // The Tree template has not named the ItemsPresenter, 
+                // so walk the descendents and find the child.
+                itemsPresenter = FindVisualChild<ItemsPresenter>(container);
+                if (itemsPresenter == null)
+                {
+                    container.UpdateLayout();
+                    itemsPresenter = FindVisualChild<ItemsPresenter>(container);
+                }
+            }
+
+            Panel itemsHostPanel = (Panel)VisualTreeHelper.GetChild(itemsPresenter, 0);
+
+            // Ensure that the generator for this panel has been created.
+            UIElementCollection children = itemsHostPanel.Children;
+
+            MyVirtualizingStackPanel virtualizingPanel = itemsHostPanel as MyVirtualizingStackPanel;
+
+            for (int i = 0, count = container.Items.Count; i < count; i++)
+            {
+                TreeViewItem subContainer;
+                if (virtualizingPanel != null)
+                {
+                    // Bring the item into view so 
+                    // that the container will be generated.
+                    virtualizingPanel.BringIntoView(i);
+                    subContainer = (TreeViewItem)container.ItemContainerGenerator.ContainerFromIndex(i);
+                }
+                else
+                {
+                    subContainer = (TreeViewItem)container.ItemContainerGenerator.ContainerFromIndex(i);
+                    // Bring the item into view to maintain the 
+                    // same behavior as with a virtualizing panel.
+                    subContainer.BringIntoView();
+                }
+
+                if (subContainer != null)
+                {
+                    // Search the next level for the object.
+                    TreeViewItem resultContainer = GetTreeViewItem(subContainer, item);
+                    if (resultContainer != null)
+                    {
+                        return resultContainer;
+                    }
+                    else
+                    {
+                        // The object is not under this TreeViewItem
+                        // so collapse it.
+                        subContainer.IsExpanded = false;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Search for an element of a certain type in the visual tree.
+    /// </summary>
+    /// <typeparam name="T">The type of element to find.</typeparam>
+    /// <param name="visual">The parent element.</param>
+    /// <returns></returns>
+    private T FindVisualChild<T>(Visual visual) where T : Visual
+    {
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(visual); i++)
+        {
+            Visual child = (Visual)VisualTreeHelper.GetChild(visual, i);
+            if (child != null)
+            {
+                T correctlyTyped = child as T;
+                if (correctlyTyped != null)
+                {
+                    return correctlyTyped;
+                }
+
+                T descendent = FindVisualChild<T>(child);
+                if (descendent != null)
+                {
+                    return descendent;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    }
+
+    public class MyVirtualizingStackPanel : VirtualizingStackPanel
+    {
+        /// <summary>
+        /// Publically expose BringIndexIntoView.
+        /// </summary>
+        public void BringIntoView(int index)
+        {
+            this.BringIndexIntoView(index);
+        }
+    }
+
+
+    public static class BindableSelectedItemHelper
+    {
+        #region Properties
+
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.RegisterAttached("SelectedItem", typeof(object), typeof(BindableSelectedItemHelper),
+            new FrameworkPropertyMetadata(null, OnSelectedItemPropertyChanged));
+
+        public static readonly DependencyProperty AttachProperty = DependencyProperty.RegisterAttached("Attach", typeof(bool), typeof(BindableSelectedItemHelper), new PropertyMetadata(false, Attach));
+
+        private static readonly DependencyProperty IsUpdatingProperty = DependencyProperty.RegisterAttached("IsUpdating", typeof(bool), typeof(BindableSelectedItemHelper));
+
+        #endregion
+
+        #region Implementation
+
+        public static void SetAttach(DependencyObject dp, bool value)
+        {
+            dp.SetValue(AttachProperty, value);
+        }
+
+        public static bool GetAttach(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(AttachProperty);
+        }
+
+        public static string GetSelectedItem(DependencyObject dp)
+        {
+            return (string)dp.GetValue(SelectedItemProperty);
+        }
+
+        public static void SetSelectedItem(DependencyObject dp, object value)
+        {
+            dp.SetValue(SelectedItemProperty, value);
+        }
+
+        private static bool GetIsUpdating(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(IsUpdatingProperty);
+        }
+
+        private static void SetIsUpdating(DependencyObject dp, bool value)
+        {
+            dp.SetValue(IsUpdatingProperty, value);
+        }
+
+        private static void Attach(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            TreeView treeListView = sender as TreeView;
+            if (treeListView != null)
+            {
+                if ((bool)e.OldValue)
+                    treeListView.SelectedItemChanged -= SelectedItemChanged;
+
+                if ((bool)e.NewValue)
+                    treeListView.SelectedItemChanged += SelectedItemChanged;
+            }
+        }
+
+        private static void OnSelectedItemPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            TreeView treeListView = sender as TreeView;
+            if (treeListView != null)
+            {
+                treeListView.SelectedItemChanged -= SelectedItemChanged;
+
+                if (!(bool)GetIsUpdating(treeListView))
+                {
+                    foreach (TreeViewItem item in treeListView.Items)
+                    {
+                        if (item == e.NewValue)
+                        {
+                            item.IsSelected = true;
+                            break;
+                        }
+                        else
+                            item.IsSelected = false;
+                    }
+                }
+
+                treeListView.SelectedItemChanged += SelectedItemChanged;
+            }
+        }
+
+        private static void SelectedItemChanged(object sender, RoutedEventArgs e)
+        {
+            TreeView treeListView = sender as TreeView;
+            if (treeListView != null)
+            {
+                SetIsUpdating(treeListView, true);
+                SetSelectedItem(treeListView, treeListView.SelectedItem);
+                SetIsUpdating(treeListView, false);
+            }
+        }
+        #endregion
+    }
+
+
+}
