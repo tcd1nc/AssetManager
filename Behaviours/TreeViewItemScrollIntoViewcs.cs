76117aa9c578a921ab3c495120801803a3705fc2
Terry <tcdj1963@gmail.com>
2020-05-26 07:35:31 +1200

    Add files via upload

---
+++ Behaviours/TreeViewItemScrollIntoViewcs.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AssetManager.Behaviours
+{
+    public class BringSelectedItemIntoViewBehavior
+    {
+        public static readonly DependencyProperty IsBringSelectedIntoViewProperty = DependencyProperty.RegisterAttached(
+            "IsBringSelectedIntoView", typeof(bool), typeof(BringSelectedItemIntoViewBehavior), new PropertyMetadata(default(bool), PropertyChangedCallback));
+
+        public static void SetIsBringSelectedIntoView(DependencyObject element, bool value)
+        {
+            element.SetValue(IsBringSelectedIntoViewProperty, value);
+        }
+
+        public static bool GetIsBringSelectedIntoView(DependencyObject element)
+        {
+            return (bool)element.GetValue(IsBringSelectedIntoViewProperty);
+        }
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var treeViewItem = dependencyObject as TreeViewItem;
+            if (treeViewItem == null)
+            {
+                return;
+            }
+
+            if (!((bool)dependencyPropertyChangedEventArgs.OldValue) &&
+                ((bool)dependencyPropertyChangedEventArgs.NewValue))
+            {
+                treeViewItem.Unloaded += TreeViewItemOnUnloaded;
+                treeViewItem.Selected += TreeViewItemOnSelected;
+            }
+        }
+
+        private static void TreeViewItemOnUnloaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var treeViewItem = sender as TreeViewItem;
+            if (treeViewItem == null)
+            {
+                return;
+            }
+
+            treeViewItem.Unloaded -= TreeViewItemOnUnloaded;
+            treeViewItem.Selected -= TreeViewItemOnSelected;
+        }
+
+        private static void TreeViewItemOnSelected(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var treeViewItem = sender as TreeViewItem;
+            treeViewItem.BringIntoView();
+        }
+    }
+
+    public static class TreeViewItemBehavior
+    {
+        #region IsBroughtIntoViewWhenSelected
+        public static bool GetIsBroughtIntoViewWhenSelected(TreeViewItem treeViewItem)
+        {
+            return (bool)treeViewItem.GetValue(IsBroughtIntoViewWhenSelectedProperty);
+        }
+        public static void SetIsBroughtIntoViewWhenSelected(
+          TreeViewItem treeViewItem, bool value)
+        {
+            treeViewItem.SetValue(IsBroughtIntoViewWhenSelectedProperty, value);
+        }
+        public static readonly DependencyProperty IsBroughtIntoViewWhenSelectedProperty =
+            DependencyProperty.RegisterAttached(
+            "IsBroughtIntoViewWhenSelected",
+            typeof(bool),
+            typeof(TreeViewItemBehavior),
+            new UIPropertyMetadata(false, OnIsBroughtIntoViewWhenSelectedChanged));
+        static void OnIsBroughtIntoViewWhenSelectedChanged(
+          DependencyObject depObj, DependencyPropertyChangedEventArgs e)
+        {
+            TreeViewItem item = depObj as TreeViewItem;
+            if (item == null)
+                return;
+            if (e.NewValue is bool == false)
+                return;
+            if ((bool)e.NewValue)
+                item.Selected += OnTreeViewItemSelected;
+            else
+                item.Selected -= OnTreeViewItemSelected;
+        }
+        static void OnTreeViewItemSelected(object sender, RoutedEventArgs e)
+        {
+            // Only react to the Selected event raised by the TreeViewItem
+            // whose IsSelected property was modified. Ignore all ancestors
+            // who are merely reporting that a descendant's Selected fired.
+            if (!Object.ReferenceEquals(sender, e.OriginalSource))
+                return;
+            TreeViewItem item = e.OriginalSource as TreeViewItem;
+            if (item != null)
+                item.BringIntoView();
+            //item.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Loaded, new DispatcherOperationCallback(ScrollItemIntoView), item);
+
+        }
+
+
+        static object ScrollItemIntoView(object sender)
+        {
+            TreeViewItem _item = sender as TreeViewItem;
+            if (_item != null)
+                _item.BringIntoView();
+            return null;
+
+        }
+        #endregion // IsBroughtIntoViewWhenSelected
+    }
+
+}
